81d8bb588ef688630cc96dbe8e25108aacce54e8
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-11-11 04:19:57 +0900

    test

---
+++ src/main/java/test/testa/TestController.java
@@ -0,0 +1,13 @@
+package test.testa;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
