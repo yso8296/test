06203aba9c293aa1934d79b8eaa0a859c99c773a
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-11-11 03:29:31 +0900

    commit

---
+++ src/main/java/test/testa/TestaApplication.java
@@ -0,0 +1,13 @@
+package test.testa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/testa/TestaApplicationTests.java
@@ -0,0 +1,13 @@
+package test.testa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
