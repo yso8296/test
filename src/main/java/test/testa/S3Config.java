b5f3a16e4dd8e687e0e7fa4d4387d2f974d2a733
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-11-12 01:03:35 +0900

    test

--- src/main/java/test/testa/S3Config.java
+++ src/main/java/test/testa/S3Config.java
@@ -5,15 +5,22 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
 
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
     @Bean
     public AmazonS3 amazonS3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials("AKIAQFC264AN4OYR7SWC", "VSSv+BRDodxHZwvXmv8buA+vEyHl62WHXYYpRR+5");
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
         return AmazonS3ClientBuilder
             .standard()
